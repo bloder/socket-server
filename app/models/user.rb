8b6b1738ab1042aa591a031ade0834379b942c25
Daniel <wowmegadenis@hotmail.com>
2016-03-12 19:07:28 -0300

    Add function to comunicate with redis

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  def user action
+  msg = { resource: 'user',
+          action: action,
+          id: self.id,
+          obj: self }
+
+  $redisClient.publish 'user-data', msg.to_json
+  end
 end
