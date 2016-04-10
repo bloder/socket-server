1807b34871b4d25abb9e0b789f15093276539e76
Daniel <wowmegadenis@hotmail.com>
2016-04-10 15:53:08 -0300

    Remove useless functions from controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,23 +6,4 @@ class UserController < ApplicationController
       format.json {render action: 'index', status: :ok}
     end
   end
-
-  def show
-  end
-
-  def new
-  @user = User.new(user_params)
-
-  respond_to do |format|
-  if @user.save
-    render json: @user
-  else
-    format.json{ render json: @user.errors, status: :unprocessable_entity }
-  end
-end
-end
-
-  def user_params
-      params.require(:user).permit(:name, :age)
-    end
 end
